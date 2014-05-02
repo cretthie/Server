c3f5c150106e27b661c131ca94ecc6c04c8e1300
Thierry <crettolthierry@ringtarget.com>
2014-05-02 10:15:58 +0200

    Server modif

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,6 +1,41 @@
 package Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server 
 {
-
+	private int port;
+	private int maxCon;
+	private ServerSocket srv;
+	private Socket client;
+	private Thread conThread;
+	public Server(int port, int maxCon)
+	{
+		this.port = port;
+		this.maxCon = maxCon;
+		try 
+		{
+			srv = new ServerSocket(port, maxCon);
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	public void runNetwork()
+	{
+		while(true)
+		{
+			try 
+			{
+				client = srv.accept();
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+	}
 }
