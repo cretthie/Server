e08d6631cb6d989b8726af786bbd550ffce7a219
Thierry <crettolthierry@ringtarget.com>
2014-05-02 09:25:25 +0200

    new Server base

---
+++ src/Chat/Server.java
@@ -0,0 +1,15 @@
+package Chat;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server 
+{
+	private int port;
+	private int maxCon;
+	private ServerSocket srvSocket;
+	private Socket clientSocket;
+	private Thread conThread;
+	
+	
+}
